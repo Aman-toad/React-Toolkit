59f5e0ca46e5a632f3a24607dcda3957c48a91cc
Aman Singh <amanaspirantsingh@gmail.com>
2025-07-26 13:01:08 +0530

    add UUID Generator and JSON to CSV components with copy functionality and error handling

--- src/pages/UuidGenerator.jsx
+++ src/pages/UuidGenerator.jsx
@@ -1,13 +1,53 @@
 import React, { useState } from 'react';
 import StarField from '../components/StarBackground';
+import {v4 as uuidv4} from 'uuid'
 
-export default function UUIDGenerator(){
-  return(
+export default function UUIDGenerator() {
+
+  const [uuid, setUuid] = useState('')
+  const [copy, setCopy] = useState('Copy')
+  const generateUuid = () => {
+    const id = uuidv4();
+    setUuid(id);    
+  }
+
+  const handleCopy = () => {
+    if (uuid === '') setError('Nothing to Copy !');
+    else {
+      navigator.clipboard.writeText(uuid);
+
+      setCopy('Copied !')
+      setTimeout(() => { setCopy('Copy') }, 1500);
+    }
+  }
+
+  return (
     <div className='relative min-h-screen'>
-      <StarField/>
+      <StarField />
 
       <div>
-        
+        <h1 className="flex justify-center items-center mb-7 font-bold text-2xl lg:text-4xl text underline">UUID GENERATOR</h1>
+
+        <div className='flex flex-col justify-center items-center'>
+          <button onClick={generateUuid} className='p-4 mt-10 text-lg font-bold bg-sky-500 rounded-sm cursor-none hover:bg-sky-600'>
+            GENERATOR UUID
+          </button>
+
+        </div>
+
+        {uuid && (
+          <div className='flex justify-center items-center mt-10'>
+            <input
+            readOnly
+            value={uuid}
+              type="text"
+              className='border border-r-0 rounded-l-xl p-3 w-1/2 lg:w-1/3'
+            />
+            <button onClick={handleCopy} className='p-3 text-lg font-bold bg-sky-500 rounded-r-sm cursor-none h-13 hover:bg-sky-600'>
+              {copy}
+            </button>
+          </div>
+        )}
       </div>
     </div>
   )
--- src/pages/jsonToCsv.jsx
+++ src/pages/jsonToCsv.jsx
@@ -1,13 +1,89 @@
 import React, { useState } from 'react';
 import StarField from '../components/StarBackground';
 
-export default function JsonToCsv(){
-  return(
-    <div className='relative min-h-screen'>
-      <StarField/>
+export default function JsonToCsv() {
 
-      <div>
-        
+  const [error, setError] = useState('')
+  const [inputJson, setInputJson] = useState('');
+  const [outputCsv, setOutputCsv] = useState('')
+  const [copy, setCopy] =useState('Copy CSV')
+
+  const handleConvert = () => {
+    try {
+      const arr = JSON.parse(inputJson);
+      if (!Array.isArray(arr) || arr.length === 0) {
+        setError('Invalid or empty JSON array')
+      }
+
+      const headers = Object.keys(arr[0]);
+      const csvRows = [];
+
+      csvRows.push(headers.join(','));
+
+      for(const row of arr){
+        const values = headers.map(header => {
+          const val = row[header];
+
+          return `"${String(val).replace(/"/g, '""')}"`;
+        }) ;
+        csvRows.push(values.join(','));
+      }
+      setOutputCsv(csvRows.join('\n'))
+
+    } catch (error) {
+      setError('Invalid Input!!')
+    }
+  }
+
+  const handleCopy = () => {
+    if (outputCsv === '') setError('Nothing to Copy !');
+    else {
+      navigator.clipboard.writeText(outputCsv);
+
+      setCopy('Copied !')
+      setTimeout(() => { setCopy('Copy CSV') }, 1500);
+    }
+  }
+
+  const handleClear = () => {
+    setInputJson(''); setOutputCsv('');
+  }
+
+  return (
+    <div className="relative min-h-screen">
+      <StarField />
+
+      <h1 className="flex justify-center items-center mt-3 mb-7 font-bold text-2xl lg:text-4xl text underline">JSON TO CSV</h1>
+
+      <div className="grid grid-cols-1 text-center content-center ">
+        {/* input textarea minified */}
+        <div>
+          <textarea name="jsonInput" value={inputJson} onChange={(e) => setInputJson(e.target.value)} rows="10" placeholder="Input JSON..." className=" border w-[80%] lg:w-[60%] p-3">
+          </textarea>
+        </div>
+
+        {/* a button  */}
+        <div >
+          <button onClick={handleConvert} className="p-3 m-1 text-xl bg-sky-600 rounded-2xl cursor-none hover:bg-sky-700 ">Convert</button>
+        </div>
+
+        {/* display formatted json format */}
+        <div>
+          <textarea value={outputCsv} readOnly name="displayJson" rows="10" placeholder="CSV text..." className="border w-[80%] lg:w-[60%] p-3">
+          </textarea>
+        </div>
+
+        <div className="flex justify-center items-center">
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:gap-6 content-center w-[60%] gap-2 items-center'>
+            <button onClick={handleCopy} className="p-3 text-lg bg-sky-600 rounded-xl cursor-none hover:bg-sky-700 ">{copy}</button>
+            <button onClick={handleClear} className="p-3 text-lg bg-sky-600 rounded-xl cursor-none hover:bg-sky-700 ">Clear</button>
+          </div>
+        </div>
+
+        {/*Error div */}
+        <div className='mb-10'>
+          {error && <p className="text-red-500 mt-3 mb-50">{error}</p>}
+        </div>
       </div>
     </div>
   )
